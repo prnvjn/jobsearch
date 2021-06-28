bf4bfb30ee0b4684e43f561ca080b09e1deff12e
Pranav <prnvjn@gmail.com>
2021-06-28 15:18:11 +0530

    filter function

--- script.js
+++ script.js
@@ -3,3 +3,61 @@ document.querySelector(".button-container")
     let text = document.getElementById("filter-jobs").value
     console.log(text)
 })
+
+
+function getJobs(){
+    return fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        return data
+    })
+}
+function filterJobs(jobs,searchText){
+    if(searchText){
+        let filteredJobs = jobs.filter(
+            job =>{
+                if(job.roleName.toLowerCase().includes(searchText) || job.type.toLowerCase().includes(searchText) || job.company.toLowerCase().includes(searchText) || job.requirements.content.toLowerCase().includes(searchText))
+                {
+                    return true
+                } else{
+                    return false
+                }
+            })
+        return filterJobs;
+    }
+    return jobs;
+
+}
+function showJobs(jobs){
+    console.log("jobs in showJobs",jobs)
+    let jobsContainer = document.querySelector(".jobs-container")
+    let jobsHTML = "";
+    jobs.forEach(job => {
+       jobsHTML += `
+       <div class="job-tile">
+         <div class="top">
+           <img src="${job.logo}" >
+           <span class="material-icons more_horiz">more_horiz</span>
+        </div>
+       <div class="rolename">
+           <span>${job.roleName}</span>
+       </div>
+           <div class="description">
+               <span>${job.requirements.content}</span>
+           </div>
+           <div class="buttons">
+               <div class="button apply-now">Apply now</div>
+               <div class="button">Message</div>
+              
+           </div>
+      
+        </div>
+       `
+    });
+jobsContainer.innerHTML=jobsHTML
+
+}
+getJobs().then(data=>{
+    showJobs(data)
+})
